11a5ac426b86298ddb8a09efcbe1b822eac8cf75
ZXY <3303057136@qq.com>
2025-10-24 16:48:40 +0800

    Revert "提交考核代码：实现终端输出Hello MA，并配置CMake编译"
    This reverts commit ff7a2e62ab21a75c8866e1ffc0e532e2cb13df7e.
    
    revert: "restore main branch"

--- main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>  // 用于输出内容的头文件（必须加）
-using namespace std; // 简化代码，不用每次写std::cout
-int main() {
-    int a;
-    // 核心功能：往终端输出“Hello MA!”，endl是换行
-    cout << "Hello MA!" << endl; 
-    return 0; // 程序正常结束的标志
-}
