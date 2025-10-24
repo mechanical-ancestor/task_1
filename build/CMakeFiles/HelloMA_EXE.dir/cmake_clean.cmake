ff7a2e62ab21a75c8866e1ffc0e532e2cb13df7e
lan <3179540362@qq.com>
2025-10-24 14:25:06 +0800

    提交考核代码：实现终端输出Hello MA，并配置CMake编译

---
+++ build/CMakeFiles/HelloMA_EXE.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HelloMA_EXE.dir/main.cpp.o"
+  "CMakeFiles/HelloMA_EXE.dir/main.cpp.o.d"
+  "HelloMA_EXE"
+  "HelloMA_EXE.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HelloMA_EXE.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/HelloMA_EXE.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for HelloMA_EXE.
+# This may be replaced when dependencies are built.
